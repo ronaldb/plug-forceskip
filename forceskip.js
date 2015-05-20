f6a7fb8ddf1865fe1a34511ccf63109d70d634a4
Ronald Bruintjes <ronaldb64@gmail.com>
2015-05-20 12:37:03 -0400

    Create forceskip.js

---
+++ forceskip.js
@@ -0,0 +1,21 @@
+API.on(API.ADVANCE, callback);
+
+function callback () {
+
+	var a = API.getMedia().cid;
+	setTimeout(function() {
+	    var b = API.getMedia().cid;
+	    if (a === b) {
+	        API.sendChat("Track stuck, force skipping!");
+	        API.moderateForceSkip();
+	    }
+	}, (API.getMedia().duration * 1000) + 5000);
+
+}
+
+API.chatLog("Now running Force Skip. Refresh page to disable");
+
+/* Basically, this will only force skip if the track is definately stuck
+   It uses the duration of the track + 5 seconds to ensure the track has
+   finished playing and does need a skip to get it changed.              */
+   
